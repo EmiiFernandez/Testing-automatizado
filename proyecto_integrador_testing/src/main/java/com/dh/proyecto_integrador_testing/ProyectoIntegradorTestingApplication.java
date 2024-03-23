9279d1304d2d6ca998231062ac1b37189bb5bca6
EmiiFernandez <e.fernandezmurgia@gmail.com>
2024-03-23 16:59:45 -0300

    readme proyecto integrador

---
+++ proyecto_integrador_testing/src/main/java/com/dh/proyecto_integrador_testing/ProyectoIntegradorTestingApplication.java
@@ -0,0 +1,13 @@
+package com.dh.proyecto_integrador_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradorTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradorTestingApplication.class, args);
+	}
+
+}
---
+++ proyecto_integrador_testing/src/test/java/com/dh/proyecto_integrador_testing/ProyectoIntegradorTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.proyecto_integrador_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
